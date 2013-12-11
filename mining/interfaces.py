709643ae561c44ac6ba8e9e0a9d1f7230ebea350
ahmedbodi <ahmed_bodi@msn.com>
2013-12-11 15:15:45 +0000

    Update interfaces.py

--- mining/interfaces.py
+++ mining/interfaces.py
@@ -17,7 +17,7 @@ dbi.init_main()
 
 class WorkerManagerInterface(object):
     def __init__(self):
-	self.worker_log = {}
+        self.worker_log = {}
         self.worker_log.setdefault('authorized', {})
         return
         
