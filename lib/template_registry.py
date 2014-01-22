b185364829bd78e7da900cd0cecdc1d6054cdc1b
Alan Penner <metsfan@alumni.cmu.edu>
2014-01-22 15:07:03 -0800

    have submitblock return hash value of submitted block

--- lib/template_registry.py
+++ lib/template_registry.py
@@ -283,19 +283,20 @@ class TemplateRegistry(object):
                             
             # 7. Submit block to the network
             serialized = binascii.hexlify(job.serialize())
-#	    if settings.BLOCK_CHECK_SCRYPT_HASH:
-#                on_submit = self.bitcoin_rpc.submitblock(serialized, scrypt_hash_hex)
-#            else:
-            #just try both serialized hash and scrypt hash when checking for block creation
-            on_submit = self.bitcoin_rpc.submitblock(serialized, block_hash_hex, scrypt_hash_hex)
+            #just try both serialized hash and scrypt hash when checking for block creation; return which one worked
+            (on_submit, valid_hash) = self.bitcoin_rpc.submitblock(serialized, block_hash_hex, scrypt_hash_hex)
 
             if on_submit:
                 self.update_block()
 
-            if settings.SOLUTION_BLOCK_HASH:
-                return (header_hex, block_hash_hex, share_diff, on_submit)
-            else:
-                return (header_hex, scrypt_hash_hex, share_diff, on_submit)
+            return (header_hex, valid_hash, share_diff, on_submit)
+
+#
+#           if settings.SOLUTION_BLOCK_HASH:
+#                return (header_hex, block_hash_hex, share_diff, on_submit)
+#            else:
+#                return (header_hex, scrypt_hash_hex, share_diff, on_submit)
+
         
         if settings.SOLUTION_BLOCK_HASH:
         # Reverse the header and get the potential block hash (for scrypt only) only do this if we want to send in the block hash to the shares table
