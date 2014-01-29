a98fbf605759dd726e9e40d5d0bb1a5a8d957c8d
Ahmed Bodiwala <ahmed_bodi@msn.com>
2014-01-29 03:52:04 +0000

    Added Spaghetti from SSOC

--- conf/config_sample.py
+++ conf/config_sample.py
@@ -181,13 +181,15 @@ WORKER_BAN_TIME = 300    # How long we temporarily ban worker
 INVALID_SHARES_PERCENT = 50    # Allow average invalid shares vary this % before we ban
 
 # ******************** E-Mail Notification Settings *********************
-NOTIFY_EMAIL_TO = ''    # Where to send Start/Found block notifications
-NOTIFY_EMAIL_TO_DEADMINER = ''  # Where to send dead miner notifications
+NOTIFY_ADMIN = 'admin@domain.com'
 NOTIFY_EMAIL_FROM = 'root@localhost'  # Sender address
-NOTIFY_EMAIL_SERVER = 'localhost'  # E-Mail Sender
-NOTIFY_EMAIL_USERNAME = ''    # E-Mail server SMTP Logon
-NOTIFY_EMAIL_PASSWORD = ''
-NOTIFY_EMAIL_USETLS = True
+NOTIFY_EMAIL_SERVER = 'localhost'  # SMTP Server
+NOTIFY_EMAIL_SERVER_PORT = '587'  # SMTP Port
+NOTIFY_EMAIL_USERNAME = ''    # SMTP Login
+NOTIFY_EMAIL_PASSWORD = ''    # SMTP Password  
+NOTIFY_EMAIL_USETLS = True    # WIP
+NOTIFY_DEADMINER_INTERVAL = 600 # Notify Dead Miners every x seconds
+NOTIFY_MAX_EMAILS = 10          # Max emails sent per user
 
 #### Memcache ####
 # Memcahce is a requirement. Enter the settings below
