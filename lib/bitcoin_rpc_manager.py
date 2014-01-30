58c12f18e2c34acdb2077d14a070d426c3ee5c51
ahmedbodi <ahmed_bodi@msn.com>
2014-01-30 09:40:45 +0000

    Update bitcoin_rpc_manager.py

--- lib/bitcoin_rpc_manager.py
+++ lib/bitcoin_rpc_manager.py
@@ -89,11 +89,11 @@ class BitcoinRPCManager(object):
                 return self.conns[self.curr_conn]._call(method,params)
             except:
                 self.next_connection()
-        def check_submitblock(self):
-            while True:
-               try:
+    def check_submitblock(self):
+        while True:
+              try:
                   return self.conns[self.curr_conn].check_submitblock()
-               except:
+              except:
                   self.next_connection()
 
     def submitblock(self, block_hex, hash_hex, scrypt_hex):
