9ba3b4f6ada7df22b585159d818a167aa4749547
ahmedbodi <ahmed_bodi@msn.com>
2014-03-03 11:26:34 +0000

    Add Options

--- algo/coindefinition.py
+++ algo/coindefinition.py
@@ -1,3 +1,9 @@
+import lib.logger
+log = lib.logger.get_logger('Coin Definition')
+log.debug("Got to Coin Definition")
+
+ALGOS = ['sha256d','ltc_scrypt','yac_scrypt','quark_hash','x11_hash']
+
 ALGORITHM = 1 
 TX_MESSAGES = True
 # Algorithm Array is as follows:
