a59a9d720fc272d54e03e6b9939530e1a684c8a5
ahmedbodi <ahmed_bodi@msn.com>
2014-03-03 12:07:03 +0000

    Add Algorithm's

--- algo/coindefinition.py
+++ algo/coindefinition.py
@@ -1,15 +1,47 @@
 import lib.logger
+import lib.settings as settings
 log = lib.logger.get_logger('Coin Definition')
 log.debug("Got to Coin Definition")
 
-ALGOS = ['sha256d','ltc_scrypt','yac_scrypt','quark_hash','x11_hash']
-
-ALGORITHM = 1 
-TX_MESSAGES = True
+ALGOS = ['ltc_scrypt',pass,'yac_scrypt','quark_hash','x11_hash','algo.skeinhash.skeinhash']
+DIFF1 = ['0x0000ffff00000000000000000000000000000000000000000000000000000000','0x000000ffff000000000000000000000000000000000000000000000000000000','0x00000000ffff0000000000000000000000000000000000000000000000000000']
 # Algorithm Array is as follows:
 # Scrypt = 1
 # SHA256 = 2 
 # YAC = 3
 # Quark = 4
-# Skein = 5
-# Adding a new algo is as simple as editing lib/coindefinition.py and adding the algorithm to there
+# X11 = 5
+# Skein = 6
+# Adding a new algo is as simple as editing lib/coindefinition.py and adding the algorithm to the array 
+
+def algo(self):
+    if settings.ALGORITHM == 1:
+         return ALGOS[1]
+    elif settings.ALGORITHM == 2:
+         return ALGOS[2]
+    elif settings.ALGORITHM == 3:
+         return ALGOS[3] 
+    elif settings.ALGORITHM == 4:
+         return ALGOS[4] 
+    elif settings.ALGORITHM == 5:
+         return ALGOS[5]
+    elif settings.ALGORITHM == 6:
+         return ALGOS[6]
+         
+def diff1(self):
+    if settings.ALGORITHM == 1:
+         return DIFF1[1]
+    elif settings.ALGORITHM == 2:
+         return DIFF1[3]
+    elif settings.ALGORITHM == 3:
+         return DIFF1[1] 
+    elif settings.ALGORITHM == 4:
+         return DIFF1[2] 
+    elif settings.ALGORITHM == 5:
+         return DIFF1[3]
+    elif settings.ALGORITHM == 6:
+         return DIFF1[3]
+         
+def header(self)
+    if settings.ALGORITHM == 1 or settings.ALGORITHM == 3 or settings.ALGORITHM == 4 
+       return True
