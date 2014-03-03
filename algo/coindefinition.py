d874c6ad6a8495c92b0b47d5641728ae00270c71
ahmedbodi <ahmed_bodi@msn.com>
2014-03-03 12:24:14 +0000

    Hybrid Sha256/Scrypt

--- algo/coindefinition.py
+++ algo/coindefinition.py
@@ -3,30 +3,33 @@ import lib.settings as settings
 log = lib.logger.get_logger('Coin Definition')
 log.debug("Got to Coin Definition")
 
-ALGOS = ['ltc_scrypt','yac_scrypt','quark_hash','x11_hash','algo.skeinhash.skeinhash']
+ALGOS = ['ltc_scrypt',None,'yac_scrypt','quark_hash','x11_hash','algo.skeinhash.skeinhash','medcoin_hybrid']
 DIFF1 = ['0x0000ffff00000000000000000000000000000000000000000000000000000000','0x000000ffff000000000000000000000000000000000000000000000000000000','0x00000000ffff0000000000000000000000000000000000000000000000000000']
 # Algorithm Array is as follows:
 # Scrypt = 1
-# SHA256 = 2 
+# SHA256 = 2(none)
 # YAC = 3
 # Quark = 4
 # X11 = 5
 # Skein = 6
+# HybridSHA256 = 7
 # Adding a new algo is as simple as editing lib/coindefinition.py and adding the algorithm to the array 
 
 def algo(self):
     if settings.ALGORITHM == 1:
          return ALGOS[1]
     elif settings.ALGORITHM == 2:
-         return None
+         return ALGOS[2]
     elif settings.ALGORITHM == 3:
-         return ALGOS[2] 
-    elif settings.ALGORITHM == 4:
          return ALGOS[3] 
+    elif settings.ALGORITHM == 4:
+         return ALGOS[4] 
     elif settings.ALGORITHM == 5:
-         return ALGOS[4]
-    elif settings.ALGORITHM == 6:
          return ALGOS[5]
+    elif settings.ALGORITHM == 6:
+         return ALGOS[6]
+    elif settings.ALGORITHM == 7:
+         return ALGOS[7]
          
 def diff1(self):
     if settings.ALGORITHM == 1:
@@ -41,7 +44,9 @@ def diff1(self):
          return DIFF1[3]
     elif settings.ALGORITHM == 6:
          return DIFF1[3]
+    elif settings.ALGORITHM == 7:
+         return DIFF1[1]
          
 def header(self):
-    if settings.ALGORITHM == 1 or settings.ALGORITHM == 3 or settings.ALGORITHM == 4 
+    if settings.ALGORITHM == 1 or settings.ALGORITHM == 3 or settings.ALGORITHM == 4 or settings.ALGORITHM == 7 
        return True
