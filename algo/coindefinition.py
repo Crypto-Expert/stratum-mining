e449b07d044638ba3550158c9d4a440a370704e4
ahmedbodi <ahmed_bodi@msn.com>
2014-03-03 11:25:21 +0000

    Coin Definitions

---
+++ algo/coindefinition.py
@@ -0,0 +1,9 @@
+ALGORITHM = 1 
+TX_MESSAGES = True
+# Algorithm Array is as follows:
+# Scrypt = 1
+# SHA256 = 2 
+# YAC = 3
+# Quark = 4
+# Skein = 5
+# Adding a new algo is as simple as editing lib/coindefinition.py and adding the algorithm to there
