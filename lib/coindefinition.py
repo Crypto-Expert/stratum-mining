e5779ff2bd6a8324ba34f60c1b48f5a1781b20c6
ahmedbodi <ahmed_bodi@msn.com>
2014-03-03 11:25:41 +0000

    Delete coindefinition.py

--- lib/coindefinition.py
+++
@@ -1,8 +0,0 @@
-import lib.logger
-log = lib.logger.get_logger('Coin Definition')
-log.debug("Got to Coin Definition")
-
-ALGOS = ['sha256d','ltc_scrypt','yac_scrypt','quark_hash','x11_hash']
-
-if ALGOS = 1:
-   DIFF1 = 
