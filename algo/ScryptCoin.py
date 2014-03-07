11a937b7bba6402292a0174f74f038233f8e1e9a
ahmedbodi <ahmed_bodi@msn.com>
2014-03-07 17:56:55 +0000

    ScryptCoin Def

---
+++ algo/ScryptCoin.py
@@ -0,0 +1,14 @@
+class Scrypt_Coin:
+		# Algorithm Needed
+		def algo(self):
+			self.algorithm = ALGOS[1]
+			return self.algorithm
+		# Difficulty 1 
+		def diff1(self):
+			self.DIFF1 = DIFF[1]
+			return self.DIFF1
+			
+		# Header modifications
+		def header(self)
+			self.header = True
+			return self.header
