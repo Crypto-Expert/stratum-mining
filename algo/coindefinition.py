b7689b861201128e5ff94331b78585e7b47394d6
Andrew Pryde <andrew@rocketpod.co.uk>
2014-03-08 13:54:25 +0000

    Cleaning up basecoin and requirements

--- algo/coindefinition.py
+++ algo/coindefinition.py
@@ -1,69 +1,70 @@
 import lib.logger
-import lib.settings as settings
-log = lib.logger.get_logger('Coin Definition')
-log.debug("Got to Coin Definition")
-from algo import *
-from hashlib import sha256
 import util
+import struct
+from util import ser_uint256
+
+
+logger = lib.logger.get_logger('Coin Definition')
+logger.debug("Got to Coin Definition")
+
 
 class BaseCoin(object):
     """
-    Base class for coins. Defines the properties that need to be overridden.
-    BaseCoin uses Bitcoin as the base algorithm. For Alt coins override any
-    relevant functions.
+        Base class for coins. Defines the properties that need to be
+        overridden.  BaseCoin uses Bitcoin as the base algorithm. For Alt coins
+        override any relevant functions.
     """
 
-    @property
     def hash_bin(self, header_bin):
         """
-        The hashing algorithm that is used for proof of work (Binary?)
-	Location: lib/template_registry.py#L232 
+            The hashing algorithm that is used for proof of work
+            Location: lib/template_registry.py#L232
+
+            Args:
+                heder_bin (str): The block header to be hashed.
         """
-        return util.doublesha(''.join([ header_bin[i*4:i*4+4][::-1] for i in range(0, 20) ]))
+        return util.doublesha(header_bin)
 
     @property
-    def calc_algo(self, nVersion, hashPrevBlock, hashMerkleRoot, nTime, nBits, nNonce):
-	"""
-	Builds Data Inside the BLOCKS
-	Location: lib/halfnode.py#L250
-	"""
-        if self.algo is None:
-           r = []
-           r.append(struct.pack("<i", nVersion))
-           r.append(ser_uint256(      hashPrevBlock))
-           r.append(ser_uint256(      hashMerkleRoot))
-           r.append(struct.pack("<I", nTime))
-           r.append(struct.pack("<I", nBits))
-           r.append(struct.pack("<I", nNonce))
-	   self.algo = uint256_from_str(SHA256.new(SHA256.new(''.join(r)).digest()).digest())
-        return self.algo
+    def calc_algo(self, nVersion, hashPrevBlock,
+                  hashMerkleRoot, nTime, nBits, nNonce):
+        """
+            Builds Data Inside the BLOCKS
+            Location: lib/halfnode.py#L250
+        """
+        r = []
+        r.append(struct.pack("<i", nVersion))
+        r.append(ser_uint256(hashPrevBlock))
+        r.append(ser_uint256(hashMerkleRoot))
+        r.append(struct.pack("<I", nTime))
+        r.append(struct.pack("<I", nBits))
+        r.append(struct.pack("<I", nNonce))
+        return ''.join(r)
 
     @property
     def diff1(self):
         """
-        The target for diff1
-	Location: lib/template_registry.py#L145 
+            The target for diff1
+            Location: lib/template_registry.py#L145
         """
-        return 0x00000000ffff0000000000000000000000000000000000000000000000000000
+        return \
+            0x00000000ffff0000000000000000000000000000000000000000000000000000
 
     @property
-    def header(self, header_hex):
-	"""
-	The Header_Hex Needs Padding?
-	Location lib/template_registry.py#L262
-	"""
-	return header_hex = header_hex
+    def header_needs_padding(self):
+        """
+        The Header_Hex Needs Padding?
+        Location lib/template_registry.py#L262
+        """
+        return False
 
     @property
     def block_hash_hex(self, header_bin):
-	"""
-	The Block Hewader
-	Location lib/template_registry.py#L289
-	"""
-	block_hash_bin = util.doublesha(''.join([ header_bin[i*4:i*4+4][::-1] for i in range(0, 20) ]))
-        block_hash_hex = block_hash_bin[::-1].encode('hex_codec')
-	return block_hash_hex
+        """
+            The Block Hewader
+            Location lib/template_registry.py#L289
 
-
-
-    
+            Args:
+                heder_bin (str): The block header to be hashed.
+        """
+        return self.block_hash(header_bin)[::-1].encode('hex_codec')
