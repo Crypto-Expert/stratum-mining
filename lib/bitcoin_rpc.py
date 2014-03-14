d453e1183743acef95469d7658855819fdf6df0a
Andrew Pryde <andrew@rocketpod.co.uk>
2014-03-14 20:08:48 +0000

    cleaning up bitcoin_rpc.py

--- lib/bitcoin_rpc.py
+++ lib/bitcoin_rpc.py
@@ -6,13 +6,13 @@ import simplejson as json
 import base64
 from twisted.internet import defer
 from twisted.web import client
-import time
 
 import lib.logger
 log = lib.logger.get_logger('bitcoin_rpc')
 
+
 class BitcoinRPC(object):
-    
+
     def __init__(self, host, port, username, password):
         log.debug("Got to Bitcoin RPC")
         self.bitcoin_url = 'http://%s:%d' % (host, port)
@@ -22,7 +22,7 @@ class BitcoinRPC(object):
             'Authorization': 'Basic %s' % self.credentials,
         }
         client.HTTPClientFactory.noisy = False
-	self.has_submitblock = False        
+        self.has_submitblock = False
 
     def _call_raw(self, data):
         client.Headers
@@ -32,91 +32,115 @@ class BitcoinRPC(object):
             headers=self.headers,
             postdata=data,
         )
-           
+
     def _call(self, method, params):
-        #log.debug("RPC Call: %s" % json.dumps({'jsonrpc': '2.0', 'method':
-            #method,'params': params,'id': '1'}))
-        return self._call_raw(json.dumps({
+        log.debug("RPC Call: %s" % json.dumps(
+            {
                 'jsonrpc': '2.0',
                 'method': method,
                 'params': params,
-                'id': '1',
-            }))
-    
+                'id': '1'
+            })
+        )
+        return self._call_raw(json.dumps({
+            'jsonrpc': '2.0',
+            'method': method,
+            'params': params,
+            'id': '1',
+        }))
+
     @defer.inlineCallbacks
     def check_submitblock(self):
         try:
             log.info("Checking for submitblock")
-            resp = (yield self._call('submitblock', []))
-	    self.has_submitblock = True
+            yield self._call('submitblock', [])
+            self.has_submitblock = True
         except Exception as e:
             if (str(e) == "404 Not Found"):
                 log.debug("No submitblock detected.")
-		self.has_submitblock = False
+                self.has_submitblock = False
             elif (str(e) == "500 Internal Server Error"):
                 log.debug("submitblock detected.")
-		self.has_submitblock = True
+                self.has_submitblock = True
             else:
                 log.debug("unknown submitblock check result.")
-		self.has_submitblock = True
+                self.has_submitblock = True
         finally:
-              defer.returnValue(self.has_submitblock)
+            defer.returnValue(self.has_submitblock)
 
-    
     @defer.inlineCallbacks
     def submitblock(self, block_hex, hash_hex, scrypt_hex):
-  #try 5 times? 500 Internal Server Error could mean random error or that TX messages setting is wrong
+        # try 5 times? 500 Internal Server Error could mean random error or
+        # that TX messages setting is wrong
         attempts = 0
         while True:
             attempts += 1
-            if self.has_submitblock == True:
+            if self.has_submitblock is True:
                 try:
-                    log.debug("Submitting Block with submitblock: attempt #"+str(attempts))
-                    log.debug([block_hex,])
-                    resp = (yield self._call('submitblock', [block_hex,]))
+                    log.debug(
+                        "Submitting Block with submitblock: attempt #" +
+                        str(attempts))
+                    log.debug([block_hex, ])
+                    resp = (yield self._call('submitblock', [block_hex, ]))
                     log.debug("SUBMITBLOCK RESULT: %s", resp)
                     break
                 except Exception as e:
                     if attempts > 4:
-                        log.exception("submitblock failed. Problem Submitting block %s" % str(e))
+                        log.exception(
+                            "submitblock failed. Problem Submitting block %s" % str(e))
                         log.exception("Try Enabling TX Messages in config.py!")
                         raise
                     else:
                         continue
             elif self.has_submitblock == False:
                 try:
-                    log.debug("Submitting Block with getblocktemplate submit: attempt #"+str(attempts))
-                    log.debug([block_hex,])
+                    log.debug(
+                        "Submitting Block with getblocktemplate submit: attempt #"+str(attempts))
+                    log.debug([block_hex, ])
                     resp = (yield self._call('getblocktemplate', [{'mode': 'submit', 'data': block_hex}]))
                     break
                 except Exception as e:
                     if attempts > 4:
-                        log.exception("getblocktemplate submit failed. Problem Submitting block %s" % str(e))
+                        log.exception(
+                            "getblocktemplate submit failed. Problem Submitting block %s" % str(e))
                         log.exception("Try Enabling TX Messages in config.py!")
                         raise
                     else:
                         continue
-            else:  # self.has_submitblock = None; unable to detect submitblock, try both
+            # self.has_submitblock = None; unable to detect submitblock, try
+            # both
+            else:
                 try:
                     log.debug("Submitting Block with submitblock")
-                    log.debug([block_hex,])
-                    resp = (yield self._call('submitblock', [block_hex,]))
+                    log.debug([block_hex, ])
+                    resp = (yield self._call('submitblock', [block_hex, ]))
                     break
                 except Exception as e:
                     try:
-                        log.exception("submitblock Failed, does the coind have submitblock?")
+                        log.exception(
+                            "submitblock Failed, does the coind have submitblock?")
                         log.exception("Trying GetBlockTemplate")
-                        resp = (yield self._call('getblocktemplate', [{'mode': 'submit', 'data': block_hex}]))
+                        yield self._call(
+                            'getblocktemplate',
+                            [
+                                {
+                                    'mode': 'submit',
+                                    'data': block_hex
+                                }
+                            ]
+                        )
                         break
                     except Exception as e:
                         if attempts > 4:
-                            log.exception("submitblock failed. Problem Submitting block %s" % str(e))
-                            log.exception("Try Enabling TX Messages in config.py!")
+                            log.exception(
+                                "submitblock failed. Problem Submitting block %s" % str(e))
+                            log.exception(
+                                "Try Enabling TX Messages in config.py!")
                             raise
                         else:
                             continue
 
-        if json.loads(resp)['result'] == None:
+        if json.loads(resp)['result'] is None:
             # make sure the block was created.
             log.info("CHECKING FOR BLOCK AFTER SUBMITBLOCK")
             defer.returnValue((yield self.blockexists(hash_hex, scrypt_hex)))
@@ -125,22 +149,23 @@ class BitcoinRPC(object):
 
     @defer.inlineCallbacks
     def getinfo(self):
-         resp = (yield self._call('getinfo', []))
-         defer.returnValue(json.loads(resp)['result'])
-    
+        resp = (yield self._call('getinfo', []))
+        defer.returnValue(json.loads(resp)['result'])
+
     @defer.inlineCallbacks
     def getblocktemplate(self):
         try:
             resp = (yield self._call('getblocktemplate', [{}]))
             defer.returnValue(json.loads(resp)['result'])
-        # if internal server error try getblocktemplate without empty {} # ppcoin
+        # if internal server error try getblocktemplate without empty {} #
+        # ppcoin
         except Exception as e:
             if (str(e) == "500 Internal Server Error"):
                 resp = (yield self._call('getblocktemplate', []))
                 defer.returnValue(json.loads(resp)['result'])
             else:
                 raise
-                                                  
+
     @defer.inlineCallbacks
     def prevhash(self):
         resp = (yield self._call('getwork', []))
@@ -149,10 +174,10 @@ class BitcoinRPC(object):
         except Exception as e:
             log.exception("Cannot decode prevhash %s" % str(e))
             raise
-        
+
     @defer.inlineCallbacks
     def validateaddress(self, address):
-        resp = (yield self._call('validateaddress', [address,]))
+        resp = (yield self._call('validateaddress', [address, ]))
         defer.returnValue(json.loads(resp)['result'])
 
     @defer.inlineCallbacks
@@ -166,7 +191,7 @@ class BitcoinRPC(object):
         blockheight = None
         # try both hash_hex and scrypt_hex to find block
         try:
-            resp = (yield self._call('getblock', [hash_hex,]))
+            resp = (yield self._call('getblock', [hash_hex, ]))
             result = json.loads(resp)['result']
             if "hash" in result and result['hash'] == hash_hex:
                 log.debug("Block found: %s" % hash_hex)
@@ -179,9 +204,9 @@ class BitcoinRPC(object):
                 log.info("Cannot find block for %s" % hash_hex)
                 defer.returnValue(False)
 
-        except Exception as e:
+        except Exception:
             try:
-                resp = (yield self._call('getblock', [scrypt_hex,]))
+                resp = (yield self._call('getblock', [scrypt_hex, ]))
                 result = json.loads(resp)['result']
                 if "hash" in result and result['hash'] == scrypt_hex:
                     valid_hash = scrypt_hex
@@ -194,25 +219,31 @@ class BitcoinRPC(object):
                     log.info("Cannot find block for %s" % scrypt_hex)
                     defer.returnValue(False)
 
-            except Exception as e:
-                log.info("Cannot find block for hash_hex %s or scrypt_hex %s" % hash_hex, scrypt_hex)
+            except Exception:
+                log.info("Cannot find block for hash_hex %s or scrypt_hex %s" %
+                         hash_hex, scrypt_hex)
                 defer.returnValue(False)
 
-        #after we've found the block, check the block with that height in the blockchain to see if hashes match
+        # after we've found the block, check the block with that height in the
+        # blockchain to see if hashes match
         try:
-            log.debug("checking block hash against hash of block height: %s", blockheight)
-            resp = (yield self._call('getblockhash', [blockheight,]))
+            log.debug("checking block hash against hash of block height: %s",
+                      blockheight)
+            resp = (yield self._call('getblockhash', [blockheight, ]))
             hash = json.loads(resp)['result']
             log.debug("hash of block of height %s: %s", blockheight, hash)
             if hash == valid_hash:
-                log.debug("Block confirmed: hash of block matches hash of blockheight")
+                log.debug(
+                    "Block confirmed: hash of block matches hash of blockheight")
                 defer.returnValue(True)
             else:
-                log.debug("Block invisible: hash of block does not match hash of blockheight")
+                log.debug(
+                    "Block invisible: hash of block does not match hash of blockheight")
                 defer.returnValue(False)
 
-        except Exception as e:
-            # cannot get blockhash from height; block was created, so return true
+        except Exception:
+            # cannot get blockhash from height; block was created, so return
+            # true
             defer.returnValue(True)
         else:
             log.info("Cannot find block for %s" % hash_hex)
