1aea432f4791840dd16b5db106a5c0f4d3783f5a
Ahmed Bodiwala <ahmedbodi@crypto-expert.com>
2017-02-07 00:32:20 +0000

    Fixed up Weird Indentation in TemplateRegistry

--- lib/template_registry.py
+++ lib/template_registry.py
@@ -284,7 +284,7 @@ class TemplateRegistry(object):
                             
             # 7. Submit block to the network
             serialized = binascii.hexlify(job.serialize())
-	        on_submit = self.bitcoin_rpc.submitblock(serialized, block_hash_hex, scrypt_hash_hex)
+            on_submit = self.bitcoin_rpc.submitblock(serialized, block_hash_hex, scrypt_hash_hex)
             if on_submit:
                 self.update_block()
 
